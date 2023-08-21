7878e4b9cd550784c0d5ba7f4094de4485b546db
Anggara <anggara.roshandi.putra5@gmail.com>
2023-08-21 12:28:50 +0700

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+const qrcode = require("qrcode-terminal");
+
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  // Generate and scan this code with your phone
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message", (msg) => {
+  if (msg.body == "!ping") {
+    client.sendMessage(msg.from, "pong");
+  }
+});
+
+client.initialize();
